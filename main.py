f1fa4233d3960c0704a9ed550e978f6cdd966c63
Lusi0 <ItsLusio@gmail.com>
2021-12-03 13:35:42 -0800

    having issues with dependancies
    going to start using poetry to solve the issues that I am having

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+from pygame.locals import *
+
+from PyOpenGL.GL import *
+from PyOpenGL.GLU import *
+
+def main():
+    pygame.init()
+    display = (500, 500)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    
+    gluPerspective(40, (display[0]/display[1]), 1, 1)
+
+    glTranslatef(0, 0, -5)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        pygame.display.flip()
+        pygame.time.wait(10)
+
+main()
